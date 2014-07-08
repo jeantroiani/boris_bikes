493076c405201d936546271fc78a5fba9e04c40c
jeantroiani <digitalguest@gmail.com>
2014-07-08 07:36:29 +0100

    4 test are running and passing

--- spec/bikes_spec.rb
+++ spec/bikes_spec.rb
@@ -2,15 +2,24 @@ require 'bikes'
 
 describe Bike do
 
+	let(:bike) { Bike.new }
+
 	it'should not be broken after we create it' do
-		the_bike = Bike.new
-		expect(the_bike).not_to be_broken
+	
+		expect(bike).not_to be_broken
 	end
 
 	it'should be able to break' do
-		the_bike = Bike.new
-		the_bike.break!
-		expect(the_bike).to be_broken
+	
+		bike.break!
+		expect(bike).to be_broken
+	end
+
+	it'should get fix' do
+		
+		bike.break!
+		bike.fix!
+		expect(bike).not_to be_broken
 	end
 
 
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,35 @@
+require 'docking_station'
+require 'bikes'
+
+describe DockingStation do
+	
+	let(:bike){Bike.new}
+	let(:station){DockingStation.new(:capacity => 20)}
+
+	
+	it 'should accept a bike' do
+		
+		expect(station.bike_count).to eq 0
+		station.dock(bike)
+		expect(station.bike_count).to eq 1
+	end
+
+	it'should release a bike' do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq 0
+	end
+
+	it'should know when the dock is full' do
+		expect(station).not_to be_full
+		20.times {station.dock(Bike.new)}
+		expect(station).to be_full
+	end
+	
+	it'should not accept a bike if it is full' do
+		20.times {station.dock(Bike.new)}
+		expect(lambda{station.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+
+end
